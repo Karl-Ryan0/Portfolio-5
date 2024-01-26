a1bc726e896f97d22dc1c750e0c0543e105d2877
Karl Ryan <kryan.ie1986@gmail.com>
2024-01-26 16:31:49 +0000

    add sales page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import sale_items
 
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('sale/', sale_items, name='sale_items',)
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ from .models import Product
 def index(request):
     products = Product.objects.all().order_by('-created_at')[:5]
     return render(request, 'home/index.html', {'products': products})
+
+
+def sale_items(request):
+    sale_items = Product.objects.filter(on_sale=True)
+    return render(request, 'home/sale_items.html', {'sale_items': sale_items})

4ce8866f3c0bf61511f912091b1ab032f8b4b1e2
Karl Ryan <kryan.ie1986@gmail.com>
2024-02-08 18:08:44 +0000

    add basic styling for toast

--- static/js/script.js
+++ static/js/script.js
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function showToast(message) {
-        const toastHTML = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000">
+        const toastHTML = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="500000">
           <div class="toast-header">
-            <strong class="me-auto">Cart Notification</strong>
+            <strong class="me-auto">Added to Cart!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
           </div>
           <div class="toast-body">

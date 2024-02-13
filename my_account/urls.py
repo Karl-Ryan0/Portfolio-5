eb4d94aaf6ccb9dbcdbc0f9ac00f1564394e4626
Karl Ryan <kryan.ie1986@gmail.com>
2024-02-13 16:00:49 +0000

    add ability for admins to view messages directly on the site

--- my_account/urls.py
+++ my_account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import profile, edit_profile
+from .views import profile, edit_profile, view_messages, delete_message
 
 urlpatterns = [
     path('profile/', profile, name='profile'),
     path('edit/', edit_profile, name='edit_profile'),
+    path('messages/', view_messages, name='view_messages'),
+    path('messages/delete/<int:message_id>/', delete_message, name='delete_message'),
 ]
--- my_account/views.py
+++ my_account/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import UserEditForm, UserProfileForm
 from .models import UserProfile
+from home.models import ContactMessage
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -16,7 +19,8 @@ def profile(request):
 @login_required
 def edit_profile(request):
 
-    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+    user_profile, created = UserProfile.objects.get_or_create(
+        user=request.user)
 
     if request.method == 'POST':
         user_form = UserEditForm(request.POST, instance=request.user)
@@ -30,9 +34,29 @@ def edit_profile(request):
         user_form = UserEditForm(instance=request.user)
         profile_form = UserProfileForm(instance=user_profile)
 
-
     return render(request, 'my_account/edit_profile.html', {
         'user_form': user_form,
         'profile_form': profile_form
     })
+
+
+@login_required
+def view_messages(request):
+    if not request.user.is_staff:
+        return redirect('home')
+
+    messages_list = ContactMessage.objects.all().order_by('-received_at')
+    paginator = Paginator(messages_list, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'my_account/messages.html', {'page_obj': page_obj})
+
+
+@login_required
+def delete_message(request, message_id):
+    if request.user.is_staff:
+        message = get_object_or_404(ContactMessage, id=message_id)
+        message.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 

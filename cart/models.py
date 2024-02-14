a1f07d93c600424099f3e894887bf6ed94b0d837
Karl Ryan <kryan.ie1986@gmail.com>
2024-02-14 18:28:10 +0000

    styled the checkout button and fixed bug preventing price from displaying

--- cart/models.py
+++ cart/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.conf import settings
 from home.models import Product
 
-# Create your models here.
-
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_price(self):
+        return sum(item.total_price() for item in self.items.all())
 
 
 class CartItem(models.Model):

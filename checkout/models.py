32dc2dd9f0cb408135a0e3e3ec8cc211a11a2b3d
Karl Ryan <kryan.ie1986@gmail.com>
2024-02-20 17:32:04 +0000

    start checkout models and views

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from home.models import Product
+from cart.models import Cart
 
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name='orders')
+    cart = models.OneToOneField(
+        Cart, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    paid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def get_cost(self):
+        return self.price * self.quantity
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderItem
+from cart.models import Cart, CartItem
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 # Create your views here.
+
+
+def checkout(request):
+    cart_id = request.session.get('cart_id')
+    cart = None
+
+    if cart_id:
+        cart = Cart.objects.filter(id=cart_id).first()
+    elif request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user, active=True).first()
+
+    if not cart:
+        return render(request, 'cart/cart_empty.html', {})
+
+    if request.method == 'POST':
+
+        order = Order.objects.create(
+            user=request.user if request.user.is_authenticated else None,
+            paid_amount=cart.total_price()
+        )
+
+        for item in cart.items.all():
+            OrderItem.objects.create(
+                order=order,
+                product=item.product,
+                price=item.product.price,
+                quantity=item.quantity
+            )
+
+        cart.active = False
+        cart.save()
+
+        if not request.user.is_authenticated:
+            del request.session['cart_id']
+
+        return redirect('checkout:order_confirmation', order_id=order.id)
+
+    return render(request, 'checkout/checkout.html', {'cart': cart})

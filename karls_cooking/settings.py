664e12b8c19d4178aa489423a326eb3d33d3ebd7
Karl Ryan <kryan.ie1986@gmail.com>
2024-04-29 16:55:49 +0000

    change email password in settings.py to test

--- karls_cooking/settings.py
+++ karls_cooking/settings.py
@@ -118,7 +118,7 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info.karls.cooking@gmail.com'
-EMAIL_HOST_PASSWORD = 'gfdkoskmvbnpigqr'
+EMAIL_HOST_PASSWORD = 'gfdk oskm vbnp igqr'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
---
+++ staticfiles/js/script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.quantity-input').forEach(input => {
+        input.addEventListener('change', function() {
+            const itemId = this.dataset.itemId;
+            const newQuantity = this.value;
+            fetch(`/cart/update_quantity/${itemId}/`, {
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({quantity: newQuantity})
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    console.log("Quantity updated");
+                }
+            });
+        });
+    });
+
+    document.querySelectorAll('.remove-item-btn').forEach(button => {
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+            const confirmRemoval = confirm('Are you sure you want to remove this item?');
+        if (confirmRemoval) {
+            this.closest('form').submit();}
+            const itemId = this.dataset.itemId;
+            const itemName = this.dataset.itemName;
+            fetch(`/cart/remove_item/${itemId}/`, {
+                method: 'DELETE',
+                headers: {
+                    'X-CSRFToken': getCookie('csrftoken'),
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    document.querySelector(`#cart-item-container-${itemId}`).remove();
+                    displayMessage(`${itemName} has been removed successfully.`);
+                }
+            });
+        });
+    });
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=') ) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}

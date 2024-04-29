ee7699fc4e4de84786858553b0474ebaf75c4ab2
Karl Ryan <kryan.ie1986@gmail.com>
2024-04-29 17:03:34 +0000

    remove email credentials and create new ones hidden in env

--- karls_cooking/settings.py
+++ karls_cooking/settings.py
@@ -117,8 +117,8 @@ SITE_ID = 1
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info.karls.cooking@gmail.com'
-EMAIL_HOST_PASSWORD = 'gfdk oskm vbnp igqr'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 

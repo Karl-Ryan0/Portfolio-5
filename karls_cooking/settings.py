7e1e068f518b4d2a3264ba44c7bd965a4e528ca2
Karl Ryan <kryan.ie1986@gmail.com>
2024-04-30 16:03:44 +0000

    add view to send order confirmation emails

--- karls_cooking/settings.py
+++ karls_cooking/settings.py
@@ -115,7 +115,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
